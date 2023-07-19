40816fb16dbd004c28ab738d02bd755e2278fa60
Hamza <maqhamxa@gmail.com>
2023-07-19 12:23:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 /* This is a
 multi-line comment */
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
